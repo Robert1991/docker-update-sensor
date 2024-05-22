d8cdc73fc768adbca5de14c49943d92cc218c892
Robert Waddell <robert.waddell@vivavis.com>
2024-05-22 08:02:08 +0200

    using docker for script to deploy running server on my home server

--- deployOnServer.sh
+++ deployOnServer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-./gradlew bootJar
-scp build/libs/docker.update.sensor-0.0.1-SNAPSHOT.jar robert@rpn-home-server:/home/robert/
+WORKING_DIR="/home/robert/automations/docker-update-sensor/"
 
-ssh robert@rpn-home-server "java -jar docker.update.sensor-0.0.1-SNAPSHOT.jar"
+./gradlew bootJar
+scp build/libs/docker.update.sensor-0.0.1-SNAPSHOT.jar robert@rpn-home-server:$WORKING_DIR/build/libs/
+scp Dockerfile robert@rpn-home-server:$WORKING_DIR/Dockerfile
+
+ssh robert@rpn-home-server "cd $WORKING_DIR && docker build -t docker-update-sensor ."
+ssh robert@rpn-home-server "docker run -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 docker-update-sensor"
