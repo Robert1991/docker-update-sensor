6900c1964dee6fee6c9de7d1a485e9c609f9b65e
Robert Waddell <robert.waddell@vivavis.com>
2024-05-23 09:54:58 +0200

    updated version to next snapshot

--- deployOnServer.sh
+++ deployOnServer.sh
@@ -3,8 +3,8 @@
 WORKING_DIR="/home/robert/automations/docker-update-sensor/"
 
 ./gradlew bootJar
-scp build/libs/docker.update.sensor-0.0.1-SNAPSHOT.jar robert@rpn-home-server:$WORKING_DIR/build/libs/
+scp build/libs/docker.update.sensor-1.0.1-SNAPSHOT.jar robert@rpn-home-server:$WORKING_DIR/build/libs/
 scp Dockerfile robert@rpn-home-server:$WORKING_DIR/Dockerfile
 
-ssh robert@rpn-home-server "cd $WORKING_DIR && docker build -t docker-update-sensor ."
-ssh robert@rpn-home-server "docker run -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 docker-update-sensor"
+ssh robert@rpn-home-server "cd $WORKING_DIR && docker build -t docker-update-sensor-test ."
+ssh robert@rpn-home-server "docker run -v /var/run/docker.sock:/var/run/docker.sock -p 8090:8080 docker-update-sensor-test"
